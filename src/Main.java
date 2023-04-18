1fd47bbcf92a808de217dd6e97e337dd6058a49d
AnnaYedoshina <yedoshina.java@gmail.com>
2023-04-18 17:17:31 +0300

    master

--- src/Main.java
+++ src/Main.java
@@ -10,30 +10,29 @@ public class Main {
         System.out.println(Arrays.toString(testChar) + Arrays.toString(testChar1));
 
         main1(s);
-        //main2(s);
+        main2(s);
         main3(s);
         main4(s);
         main5(s);
         main6(s);
+        main7(s);
     }
 
     public static void main1(String s) {
-        String xx = "1234 - abc - 4321 - cba - 1a2b";
-        xx = xx.replace("a", "*").replace("b", "*").replace("c", "*");
-        System.out.println(xx);
+        s = s.replace("a", "*")
+                .replace("b", "*")
+                .replace("c", "*");
+        System.out.println(s);
     }
 
-//    public static void main2(String s) {
-//        String xx = "1234 - abc - 4321 - cba - 1a2b";
-//        String[] split = xx.split("/");
-//        String xx1 = split[1] + split[3] + split[5];
-//        System.out.println(xx1);
-//
-//
-//    }
+    public static void main2(String s) {
+        String join = String.join("/","abc","cba", "a", "b");
+
+        System.out.println(join);
+
+    }
 
     public static void main3(String s) {
-        String xx = "1234 - abc - 4321 - cba - 1a2b";
         StringBuilder sb = new StringBuilder(s);
         sb.insert(0, "Letters:");
         String string = sb.toString();
@@ -41,13 +40,11 @@ public class Main {
     }
 
     public static void main4(String s) {
-        String xx = "1234 - abc - 4321 - cba - 1a2b";
         boolean abc = s.contains("abc");
         System.out.println(abc);
     }
 
     public static void main5(String s) {
-        String xx = "1234 - abc - 4321 - cba - 1a2b";
         boolean start = s.startsWith("555");
         System.out.println(start);
 
@@ -60,6 +57,13 @@ public class Main {
 
     }
 
+    public static void main7(String s) {
+        String[] split = s.split("-");
+        String s1 = split[1] + split[3] + split[4];
+        System.out.println(s1.toLowerCase());
+
+
+    }
 
 
 }
